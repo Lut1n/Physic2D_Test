b4e2b6f17aba4c6121d4e87948220c6fc44f1de0
Mathieu Boulet <mathieu.boulet.44@gmail.com>
2019-08-03 11:10:30 +0200

    add CMakeLists; fix main.cpp indentations

--- main.cpp
+++ main.cpp
@@ -62,53 +62,53 @@ struct EntityRenderer
 
 int main(int argc, char* argv[])
 {
-	// create the window
-	sf::RenderWindow window(sf::VideoMode(512, 512), "Skeletal-Test");
-	EntityRenderer renderer(&window);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(512, 512), "Skeletal-Test");
+    EntityRenderer renderer(&window);
     
-	PhysicEngine phyEngine;
-	phyEngine.addEntity( new RectEntity(Vec2(230.f, 250.f),50.f,50.f) );
-	phyEngine.addEntity( new RectEntity(Vec2(240.f, 200.f),30.f,30.f) );
-	
-	phyEngine.addEntity( new RectEntity(Vec2(200.f, 195.f),20.f,50.f) );
-	phyEngine.addEntity( new RectEntity(Vec2(250.f, 190.f),24.f,14.f) );
-	phyEngine.addEntity( new RectEntity(Vec2(150.f, 110.f),18.f,33.f) );
-	phyEngine.addEntity( new RectEntity(Vec2(300.f, 150.f),54.f,108.f) );
-	
-	
-	phyEngine.addEntity( new CircleEntity(Vec2(204.f, 115.f),5.f) );
-	phyEngine.addEntity( new CircleEntity(Vec2(200.f, 100.f),10.f) );
-	phyEngine.addEntity( new CircleEntity(Vec2(198.f, 105.f),7.f) );
-	phyEngine.addEntity( new CircleEntity(Vec2(196.f, 90.f),8.f) );
-	phyEngine.addEntity( new CircleEntity(Vec2(199.f, 55.f),14.f) );
-	phyEngine.addEntity( new CircleEntity(Vec2(203.f, 70.f),12.f) );
-	
-	phyEngine.addEntity( new BoxEntity(450.f, 450.f, 30.f, Vec2(250.f,250.f), 0.f) );
-	
-	sf::Clock clock;
-	
-	// run the main loop
-	while (window.isOpen())
-	{
-		// handle events
-		sf::Event event;
-		while (window.pollEvent(event))
-			if(event.type == sf::Event::Closed) window.close();
-		
-		// update
-		float elapsed_ms = clock.getElapsedTime().asMilliseconds();
-		if(elapsed_ms >= 1.0)
-		{
-			float elapsed_sec = elapsed_ms * 0.001;
-			phyEngine.updateEntities( elapsed_sec );
-			clock.restart();
-		}
-		
-		// draw
-		window.clear();
-		for(auto& e : phyEngine.entities) renderer.draw(e);
-		window.display();
-	}
+    PhysicEngine phyEngine;
+    phyEngine.addEntity( new RectEntity(Vec2(230.f, 250.f),50.f,50.f) );
+    phyEngine.addEntity( new RectEntity(Vec2(240.f, 200.f),30.f,30.f) );
+    
+    phyEngine.addEntity( new RectEntity(Vec2(200.f, 195.f),20.f,50.f) );
+    phyEngine.addEntity( new RectEntity(Vec2(250.f, 190.f),24.f,14.f) );
+    phyEngine.addEntity( new RectEntity(Vec2(150.f, 110.f),18.f,33.f) );
+    phyEngine.addEntity( new RectEntity(Vec2(300.f, 150.f),54.f,108.f) );
+    
+    
+    phyEngine.addEntity( new CircleEntity(Vec2(204.f, 115.f),5.f) );
+    phyEngine.addEntity( new CircleEntity(Vec2(200.f, 100.f),10.f) );
+    phyEngine.addEntity( new CircleEntity(Vec2(198.f, 105.f),7.f) );
+    phyEngine.addEntity( new CircleEntity(Vec2(196.f, 90.f),8.f) );
+    phyEngine.addEntity( new CircleEntity(Vec2(199.f, 55.f),14.f) );
+    phyEngine.addEntity( new CircleEntity(Vec2(203.f, 70.f),12.f) );
+    
+    phyEngine.addEntity( new BoxEntity(450.f, 450.f, 30.f, Vec2(250.f,250.f), 0.f) );
+    
+    sf::Clock clock;
+    
+    // run the main loop
+    while (window.isOpen())
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+            if(event.type == sf::Event::Closed) window.close();
+        
+        // update
+        float elapsed_ms = clock.getElapsedTime().asMilliseconds();
+        if(elapsed_ms >= 1.0)
+        {
+            float elapsed_sec = elapsed_ms * 0.001;
+            phyEngine.updateEntities( elapsed_sec );
+            clock.restart();
+        }
+        
+        // draw
+        window.clear();
+        for(auto& e : phyEngine.entities) renderer.draw(e);
+        window.display();
+    }
 
-	return 0;
+    return 0;
 }
